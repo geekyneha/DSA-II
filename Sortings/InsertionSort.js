67bd4b3f5dfe1d8528e9f961ddaf0249ff5a3f2a
geekyneha <nehatualea@gmail.com>
2026-04-04 22:11:26 +0530

    Insertion Sort code

---
+++ Sortings/InsertionSort.js
@@ -0,0 +1,19 @@
+let insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let curr = arr[i];
+    let prev = i - 1;
+
+    while (prev >= 0 && arr[prev] > curr) {
+      arr[prev + 1] = arr[prev];
+      prev--;
+    }
+
+    arr[prev + 1] = curr;
+  }
+};
+
+let arr = [7, 4, 3, 5, 1, 2, 6];
+
+insertionSort(arr);
+
+console.log(arr);
